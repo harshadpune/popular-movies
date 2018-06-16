03b1a97c73f31015c2bac41fd3c4a14ac1f8fa1a
harshadpune <harshadpune@gmail.com>
2018-06-16 09:47:37 +0530

    Popular Movies App : Initial commit

---
+++ app/src/main/java/com/udacity/popularmovies/PopularMovies.java
@@ -0,0 +1,13 @@
+package com.udacity.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PopularMovies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popular_movies);
+    }
+}

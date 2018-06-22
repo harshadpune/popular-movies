8a16b4047f9fee38623e6bc53c828b9b1c2b3d49
harshadpune <harshadpune@gmail.com>
2018-06-22 10:30:33 +0530

    Popular Movies : Stage 1 : Date formatting added for release date

--- app/src/main/java/com/udacity/popularmovies/MovieDescriptionActivity.java
+++ app/src/main/java/com/udacity/popularmovies/MovieDescriptionActivity.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
-
 import com.squareup.picasso.Picasso;
 import com.udacity.popularmovies.databinding.ActivityMovieDetailsBinding;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by HARSHAD on 16/06/2018.
@@ -18,6 +20,9 @@ import com.udacity.popularmovies.databinding.ActivityMovieDetailsBinding;
 public class MovieDescriptionActivity extends AppCompatActivity{
 
     private ActivityMovieDetailsBinding activityMovieDetailsBinding;
+    private final String SOURCE_DATE_FORMAT = "yyyy-mm-dd";
+    private final String TARGET_DATE_FORMAT = "dd MMM yyyy";
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,7 +38,7 @@ public class MovieDescriptionActivity extends AppCompatActivity{
         Picasso.with(this).load(AppConstants.POSTER_BASE_URL+""+moviesData.getMoviePosterLink()).into(activityMovieDetailsBinding.ivMoviePhoto);
         activityMovieDetailsBinding.tvTitle.setText(moviesData.getTitle());
         activityMovieDetailsBinding.tvOverview.setText(moviesData.getPlotSynopsis());
-        activityMovieDetailsBinding.tvReleaseDate.setText(moviesData.getReleaseDate());
+        activityMovieDetailsBinding.tvReleaseDate.setText(formatDate(moviesData.getReleaseDate()));
         activityMovieDetailsBinding.tvRating.setText(""+moviesData.getVoteAverage()+"/10");
         activityMovieDetailsBinding.ratingBar.setRating(Float.parseFloat(""+moviesData.getVoteAverage()));
 
@@ -44,5 +49,17 @@ public class MovieDescriptionActivity extends AppCompatActivity{
                 activityMovieDetailsBinding.ivPallet.setBackgroundColor(palette.getVibrantColor(getResources().getColor(R.color.black)));
             }
         });
+    }
+
+    private String formatDate(String releaseDate){
+        SimpleDateFormat inputDate = new SimpleDateFormat(SOURCE_DATE_FORMAT);
+        Date sourceDate = null;
+        try {
+            sourceDate = inputDate.parse(releaseDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        SimpleDateFormat changedDate = new SimpleDateFormat(TARGET_DATE_FORMAT);
+        return changedDate.format(sourceDate);
     }
 }

114e3c289afa79b65c3ed507e6699db7f79a6944
harshadpune <harshadpune@gmail.com>
2018-06-24 21:46:49 +0530

    Popular Movies : Stage 1 : Review comments acted on 2

--- app/src/main/java/com/udacity/popularmovies/MovieDescriptionActivity.java
+++ app/src/main/java/com/udacity/popularmovies/MovieDescriptionActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
 import android.widget.Toast;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 import com.udacity.popularmovies.databinding.ActivityMovieDetailsBinding;
 import java.text.ParseException;
@@ -36,27 +37,34 @@ public class MovieDescriptionActivity extends AppCompatActivity{
     private void initComponents() {
         Bundle data = getIntent().getExtras();
         MoviesData moviesData = null;
-        if(data != null && data.containsKey((AppConstants.MOVIE_POSITION))) {
+        if (data != null && data.containsKey((AppConstants.MOVIE_POSITION))) {
             moviesData = (MoviesData) data.getSerializable(AppConstants.MOVIE_POSITION);
         }
-        if(moviesData !=null) {
+        if (moviesData != null) {
             activityMovieDetailsBinding = DataBindingUtil.setContentView(this, R.layout.activity_movie_details);
-            Picasso.with(this).load(AppConstants.POSTER_BASE_URL + "" + moviesData.getMoviePosterLink()).into(activityMovieDetailsBinding.ivMoviePhoto);
+            Picasso.with(this).load(AppConstants.POSTER_BASE_URL + "" + moviesData.getMoviePosterLink()).into(activityMovieDetailsBinding.ivMoviePhoto, new Callback() {
+                @Override
+                public void onSuccess() {
+                    Palette.from(((BitmapDrawable) activityMovieDetailsBinding.ivMoviePhoto.getDrawable()).getBitmap()).generate(new Palette.PaletteAsyncListener() {
+                        @Override
+                        public void onGenerated(Palette palette) {
+                            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(palette.getVibrantColor(getResources().getColor(R.color.black))));
+                            activityMovieDetailsBinding.ivPallet.setBackgroundColor(palette.getVibrantColor(getResources().getColor(R.color.black)));
+                        }
+                    });
+                }
+
+                @Override
+                public void onError() {
+                    // do nothing. Shows default background
+                }
+            });
             activityMovieDetailsBinding.tvTitle.setText(moviesData.getTitle());
             activityMovieDetailsBinding.tvOverview.setText(moviesData.getPlotSynopsis());
             activityMovieDetailsBinding.tvReleaseDate.setText(formatDate(moviesData.getReleaseDate()));
             activityMovieDetailsBinding.tvRating.setText("" + moviesData.getVoteAverage() + "/10");
             activityMovieDetailsBinding.ratingBar.setRating(Float.parseFloat("" + moviesData.getVoteAverage()));
 
-            if(((BitmapDrawable) activityMovieDetailsBinding.ivMoviePhoto.getDrawable()).getBitmap() !=null ) {
-                Palette.from(((BitmapDrawable) activityMovieDetailsBinding.ivMoviePhoto.getDrawable()).getBitmap()).generate(new Palette.PaletteAsyncListener() {
-                    @Override
-                    public void onGenerated(Palette palette) {
-                        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(palette.getVibrantColor(getResources().getColor(R.color.black))));
-                        activityMovieDetailsBinding.ivPallet.setBackgroundColor(palette.getVibrantColor(getResources().getColor(R.color.black)));
-                    }
-                });
-            }
         }else{
             Toast.makeText(this, ""+getString(R.string.movie_error), Toast.LENGTH_SHORT).show();
             finish();
